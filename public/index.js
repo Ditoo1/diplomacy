b0548c21931f3256f696f3e5b303666404a86c22
Ditoo1 <revecod.ignacio@gmail.com>
2024-05-27 19:05:51 -0400

    sasa

---
+++ public/index.js
@@ -0,0 +1,49 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC643BZysfS-TkAo5sUhmRREwE9MurGoD8",
+    authDomain: "democracy-a62c1.firebaseapp.com",
+    databaseURL: "https://democracy-a62c1-default-rtdb.firebaseio.com",
+    projectId: "democracy-a62c1",
+    storageBucket: "democracy-a62c1.appspot.com",
+    messagingSenderId: "1092075734405",
+    appId: "1:1092075734405:web:f8c9320c60104acc8370ec"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  const db = firebase.database();
+  
+  const username = prompt("Please Tell Us Your Name");
+  
+  function sendMessage(e) {
+    e.preventDefault();
+  
+    // get values to be submitted
+    const timestamp = Date.now();
+    const messageInput = document.getElementById("message-input");
+    const message = messageInput.value;
+  
+    // clear the input box
+    messageInput.value = "";
+  
+    //auto scroll to bottom
+    document
+      .getElementById("messages")
+      .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+  
+    // create db collection and send in the data
+    db.ref("messages/" + timestamp).set({
+      username,
+      message,
+    });
+  }
+  
+  const fetchChat = db.ref("messages/");
+  
+  fetchChat.on("child_added", function (snapshot) {
+    const messages = snapshot.val();
+    const message = `<li class=${
+      username === messages.username ? "sent" : "receive"
+    }><span>${messages.username}: </span>${messages.message}</li>`;
+    // append the message on the page
+    document.getElementById("messages").innerHTML += message;
+  });   
