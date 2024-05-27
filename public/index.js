dfbc62126edb7ef60a464874b0bd07276cfe1989
Ditoo1 <revecod.ignacio@gmail.com>
2024-05-27 19:08:20 -0400

    sasa

--- public/index.js
+++ public/index.js
@@ -6,44 +6,44 @@ var firebaseConfig = {
     storageBucket: "democracy-a62c1.appspot.com",
     messagingSenderId: "1092075734405",
     appId: "1:1092075734405:web:f8c9320c60104acc8370ec"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  
-  const db = firebase.database();
-  
-  const username = prompt("Please Tell Us Your Name");
-  
-  function sendMessage(e) {
-    e.preventDefault();
-  
-    // get values to be submitted
-    const timestamp = Date.now();
-    const messageInput = document.getElementById("message-input");
-    const message = messageInput.value;
-  
-    // clear the input box
-    messageInput.value = "";
-  
-    //auto scroll to bottom
-    document
-      .getElementById("messages")
-      .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-  
-    // create db collection and send in the data
-    db.ref("messages/" + timestamp).set({
-      username,
-      message,
-    });
-  }
-  
-  const fetchChat = db.ref("messages/");
-  
-  fetchChat.on("child_added", function (snapshot) {
-    const messages = snapshot.val();
-    const message = `<li class=${
-      username === messages.username ? "sent" : "receive"
-    }><span>${messages.username}: </span>${messages.message}</li>`;
-    // append the message on the page
-    document.getElementById("messages").innerHTML += message;
-  });   
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+
+const username = prompt("Please Tell Us Your Name");
+
+function sendMessage(e) {
+  e.preventDefault();
+
+  // get values to be submitted
+  const timestamp = Date.now();
+  const messageInput = document.getElementById("message-input");
+  const message = messageInput.value;
+
+  // clear the input box
+  messageInput.value = "";
+
+  //auto scroll to bottom
+  document
+    .getElementById("messages")
+    .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+
+  // create db collection and send in the data
+  db.ref("messages/" + timestamp).set({
+    username,
+    message,
+  });
+}
+
+const fetchChat = db.ref("messages/");
+
+fetchChat.on("child_added", function (snapshot) {
+  const messages = snapshot.val();
+  const message = `<li class=${
+    username === messages.username ? "sent" : "receive"
+  }><span>${messages.username}: </span>${messages.message}</li>`;
+  // append the message on the page
+  document.getElementById("messages").innerHTML += message;
+});
